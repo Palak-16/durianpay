2451a988cb55826e1dd28815f9f563ee2cd05e6f
Palak-16 <79013214+Palak-16@users.noreply.github.com>
2023-11-23 12:23:50 +0530

    edited index file

--- db_con.php
+++ db_con.php
@@ -8,5 +8,4 @@ $conn = mysqli_connect($server_name ,$username ,$password ,$database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
   }
-  echo "Connected successfully";
 ?>
--- index.php
+++ index.php
@@ -1,19 +1,49 @@
 <!DOCTYPE html>
-<html>
-<title>Price Compare Site</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+<form action="index.php"  method="post">
+ 
+<label class="">Search Product</label>
+<input class="" name="searchdata" type="text" maxlength="20" required><br>
+ 
+<label >Filter:</label>
+<select name="filter">
+  <option value="none">none</option>
+  <option value="highest price">highest price</option>
+  <option value="lowest price">lowest price</option>
+  <option value="review">review</option>
+  <option value="rating">rating</option>
+</select><br>
+<label class="">Top n :</label>
+<input type="number" name="topn" min="1" max="5"><br>
 
-<header class="w3-container w3-teal">
-  <h1>Price Compare Site</h1>
-</header>
-
-<form class="w3-container w3-card-4" action="index.php" method="get">
- <p>
- <input class="w3-input" name="searchdata" type="text" required>
- <label class="w3-label w3-validate">Search Product</label></p>
- <p>
- <input id="search" class="w3-input" type="submit" value="Search">
+<label >Comparison websites:</label><br>
+<input type="checkbox" name="websites[]" value="shopclues" checked>
+<label for="shopclues"> shopclues</label><br>
+<input type="checkbox" name="websites[]" value="reliance" checked>
+<label for="reliance">reliance</label><br>
+<input type="checkbox" name="websites[]" value="gadgets now" checked>
+<label for="gadgets now"> gadgets now</label><br>
+<br>
+ <input id="search" class="" type="submit" value="Search">
 </form>
+</body>
+</html>
+
+<?php
+   $website = $_POST['websites'];
+   
+     $N = count($website);
+     for($i=0; $i < $N; $i++)
+     {
+       echo($website[$i] . " ");
+     }
+?>
 
 <?php
 require('db_con.php');
